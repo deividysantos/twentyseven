8783430fc07a6e9587329a71f04e2b3c6b60a13a
Deividy <deividyosantos27@gmail.com>
2024-04-29 21:45:51 -0400

    feature: add route to create user

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
---
+++ api/app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'phone_number' => 'required|unique:users',
+            'password' => 'required|max:12',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'the :attribute field is required',
+            'max' => 'the length of :attribute must be up to :max',
+            'unique' => 'already exists a record with this value in the field :attribute',
+            'email' => 'this field must be a email'
+        ];
+    }
+}
